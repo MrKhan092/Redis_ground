a4aaa643af009d510bf7e7535855de042f110514
Mohammad Kaif <kaifm0198@gmail.com>
2026-09-12 00:51:12 +0530

    worked with list

---
+++ list.js
@@ -0,0 +1,10 @@
+const client=require('./client')
+
+async function init() {
+    // await client.lpush('messages',1);
+    // await client.lpush('messages',2);
+    // await client.lpush('messages',3);
+    const result=await client.blpop('messages',40)
+    console.log(result)
+}
+init()
--- string.js
+++ string.js
@@ -2,7 +2,7 @@ const client=require('./client')
 
 async function init() {
     // await client.set('msg:3','hey from nodejs')
-    await client.expire('msg:3',10)
+    // await client.expire('msg:3',10)
     const result=await client.get('msg:3')
     console.log("Result->",result)
 }
